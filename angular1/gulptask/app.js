4a41f13c1b996407b82484069066773674428c30
Raul Morais <raulmmorais@gmail.com>
2018-09-11 22:08:12 -0300

    Finalização do build.

--- angular1/gulptask/app.js
+++ angular1/gulptask/app.js
@@ -8,20 +8,18 @@ const htmlmin = require('gulp-htmlmin')
 gulp.task('app', ['app.html', 'app.css', 'app.js', 'app.assets'])
 
 gulp.task('app.html', ()=>{
-    return gulp.src('app/**/*.html')
-        .pipe(htmlmin({collapseWhitespace: true}))
+    return gulp.src('app/**/*.html')//.pipe(htmlmin({collapseWhitespace: true}))
         .pipe(gulp.dest('public'))
 })
 gulp.task('app.css', ()=>{
-    return gulp.src('app/**.*.css')
+    return gulp.src('app/**/*.css')
         .pipe(uglifycss({"uglyComments": true}))
         .pipe(concat('app.min.css'))
         .pipe(gulp.dest('public/assets/css'))
 })
 gulp.task('app.js', ()=>{
     return gulp.src('app/**/*.js')
-        .pipe(babel({presets:['env']}))
-        .pipe(uglify())
+        .pipe(babel({presets:['env']}))//.pipe(uglify())
         .pipe(concat('app.min.js'))
         .pipe(gulp.dest('public/assets/js'))
 })
