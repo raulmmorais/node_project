11184cc16e04315e336cba2490c393681d3b70c9
Raul Morais <raulmmorais@gmail.com>
2018-09-17 09:09:00 -0300

    Continuação.

--- angular1/app/billingCycle/billingCycleController.js
+++ angular1/app/billingCycle/billingCycleController.js
@@ -1,21 +1,22 @@
 (function () {
-    angular.module("primeiraApp").controller('BillingCycleCrl', [
+    angular.module('primeiraApp').controller('BillingCycleCrl', [
         '$http',
         BillingCycleController
     ])
-    function BillingCycleController() {
+    function BillingCycleController($http) {
         const vm = this
+        const url = 'http://localhost:3003/api/billingCyles'
+
+        vm.refresh = function(){
+          
+        }
 
         vm.create = function () {
-            const url = 'http://localhost:3003/api/billingSummary'
-
-            $http({
-                method: 'POST',
-                url : url,
-                data: vm.billingCycle,
-            }).then(function (response) {
+            $http.post(url, vm.billingCycle).then(function (response) {
                 vm.billingCycle = {}
                 console.log('Success')
+            }).catch(function(response){
+              console.log(response.data.errors)
             })
         }
     }
