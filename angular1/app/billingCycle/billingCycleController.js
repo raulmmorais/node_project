1eb71f0cc6b9c08fb8f6785e62c0ce047bb65e9a
Raul Morais <raulmmorais@gmail.com>
2018-09-19 11:02:07 -0300

    Criação dos modulos de deleção e alteração. Correção no sistema de mensagens.

--- angular1/app/billingCycle/billingCycleController.js
+++ angular1/app/billingCycle/billingCycleController.js
@@ -2,23 +2,60 @@
     angular.module('primeiraApp').controller('BillingCycleCtrl', [
         '$http',
         'msgs',
+        'tabs',
         BillingCycleController
     ])
-    function BillingCycleController($http, msgs) {
+    function BillingCycleController($http, msgs, tabs) {
         const vm = this
         const url = 'http://localhost:3003/api/billingCycles'
 
         vm.refresh = function(){
+          $http.get(url).then(function(response){
             vm.billingCycle = {}
+            vm.billingCycles = response.data
+            tabs.show(vm, {tabList: true, tabCreate: true})
+          }).catch(function(response) {
+            console.log(response.data)
+            msgs.addError(response.data.errors)
+          })
         }
 
         vm.create = function () {
           $http.post(url, vm.billingCycle).then(function(response){
-            refresh()
             msgs.addSuccess("Operação Realizada com Sucesso")
+            vm.refresh()
           }).catch(function(response){
+            console.log(response)
             msgs.addError(response.data.errors)
           })
         }
+
+        vm.update = function(){
+           const updateURL = `${url}/${vm.billingCycle._id}`
+          $http.put(updateURL, vm.billingCycle).then(function(response){
+            msgs.addSuccess("Ciclo Atualizado Com Sucesso")
+            vm.refresh()
+          })
+        }
+
+        vm.delete = function(){
+          const deleteUrl = `${url}/${vm.billingCycle._id}`
+          $http.delete(deleteUrl, vm.billingCycle).then(function(response){
+            msgs.addSuccess("Ciclo Deletado Com Sucesso")
+            vm.refresh()
+          })
+        }
+
+        vm.showTabUpdate = function(billingCycle){
+          vm.billingCycle = billingCycle
+          tabs.show(vm, {tabUpdate: true})
+        }
+
+        vm.showTabDelete = function(billingCycle){
+          vm.billingCycle = billingCycle
+          tabs.show(vm, {tabDelete: true})
+        }
+
+        vm.refresh()
     }
 })()
--- angular1/app/commom/components/factories/msgsFactory.js
+++ angular1/app/commom/components/factories/msgsFactory.js
@@ -1,6 +1,6 @@
 (function(){
   angular.module('primeiraApp').factory('msgs',[
-    'tastr',
+    'toastr',
     MsgsFactory
   ])
   function MsgsFactory(toastr){
